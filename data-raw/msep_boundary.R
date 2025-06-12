b6852beba4f25935bc2072b90a732dbf63c87044
Kim Cressman <kc2741@msstate.edu>
2025-06-12 12:26:13 -0500

    made smaller version of msep_boundary

--- data-raw/msep_boundary.R
+++ data-raw/msep_boundary.R
@@ -5,7 +5,8 @@ msep_boundary <- st_read(here::here("data-raw",
                                     "MSEP_outline.shp"))  |>
     dplyr::select(sourcrg,
                   loaddat,
-                  geometry)
+                  geometry) |>
+    st_simplify(dTolerance = 1)
 
 usethis::use_data(msep_boundary, overwrite = TRUE,
                   compress = "xz")
