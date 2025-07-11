4789afa7c0c201c756e92723266a155af476f1ad
Kim Cressman <kc2741@msstate.edu>
2025-07-11 13:43:07 -0500

    updated msep_boundary data object

--- data-raw/msep_boundary.R
+++ data-raw/msep_boundary.R
@@ -1,12 +1,17 @@
+library(msepBoundaries)
 library(sf)
 library(dplyr)
 
-msep_boundary <- st_read(here::here("data-raw",
-                                    "MSEP_outline.shp"))  |>
-    dplyr::select(sourcrg,
-                  loaddat,
-                  geometry) |>
-    st_simplify(dTolerance = 1)
+msep_boundary <- outline_full |>
+    st_simplify(dTolerance = 500)
+
+
+mapview::mapview(outline_full,
+                 alpha.regions = 0) +
+    mapview::mapview(msep_boundary,
+                     col.regions = "red",
+                     color = "red",
+                     alpha.regions = 0)
 
 usethis::use_data(msep_boundary, overwrite = TRUE,
                   compress = "xz")
