dcd8e874ac6d7f14da23527245a5dd2177f85f68
Kim Cressman <kc2741@msstate.edu>
2026-01-09 11:54:48 -0600

    changed some test expectations to shortcuts

--- tests/testthat/test-get_mssnd_data.R
+++ tests/testthat/test-get_mssnd_data.R
@@ -5,10 +5,10 @@ test_that("function gets salinity data", {
 
     # it downloaded salinity
     downloaded_var <- attributes(tmp$data)$variableInfo$variableName
-    expect_true(stringr::str_detect(downloaded_var, "Salinity"))
+    expect_match(downloaded_var, "Salinity")
 
     # there are 3 data frames in the list (i.e. it calculated daily mean sal)
-    expect_equal(length(tmp), 3)
+    expect_length(tmp, 3)
 
 })
 
@@ -26,12 +26,12 @@ test_that("function customization works", {
 
     # make sure it downloaded water temp
     downloaded_var <- attributes(tmp$data)$variableInfo$variableName
-    expect_true(stringr::str_detect(downloaded_var, "Temperature, water"))
+    expect_match(downloaded_var, "Temperature, water")
 
     # make sure it downloaded data from 2 stations
-    expect_equal(length(unique(tmp$data$site_no)), 2)
+    expect_length(unique(tmp$data$site_no), 2)
 
     # there are 2 data frames in the list (no daily mean calcs for non-salinity)
-    expect_equal(length(tmp), 2)
+    expect_length(tmp, 2)
 })
 
